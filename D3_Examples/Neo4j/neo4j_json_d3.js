d908362792a34ebb97c921756a560a4c1f3a2688
DarthNeo <morrissoid.james@hotmail.com>
2015-06-15 20:27:26 -0400

    New, but not...

---
+++ D3_Examples/Neo4j/neo4j_json_d3.js
@@ -0,0 +1,23 @@
+function idIndex(a,id)
+{
+    for (var i=0;i<a.length;i++)
+    {
+        if (a[i].id == id)
+            return i;
+    }
+    return null;
+}
+var nodes=[];
+var links=[];
+res.results[0].data.forEach(function (row)
+{
+    row.graph.nodes.forEach(function (n) {
+        if (idIndex(nodes,n.id) == null)
+            nodes.push({id:n.id,label:n.labels[0],title:n.properties.name});
+    });
+
+    links = links.concat( row.graph.relationships.map(function(r) {
+        return {start:idIndex(nodes,r.startNode),end:idIndex(nodes,r.endNode),type:r.type};
+    }));
+});
+viz = {nodes:nodes, links:links};
